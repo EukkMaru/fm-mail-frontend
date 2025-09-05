9f1fcc7dc9c98cb652a1f2d19b6a94188daaeb15
Sunoh Ryu <maru@debian.local>
2025-09-05 15:03:28 +0900

    Login functionality

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,38 @@
 <script setup>
-import { onMounted } from 'vue'
-import { gapi } from 'gapi-script'
+import { ref, onMounted } from 'vue'
 
-const clientId = '343822659955-sjqmuqskg0s15buvhfm8ndj0u0p6hcba.apps.googleusercontent.com'
-const scopes = 'https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send'
-
-function start() {
-  gapi.client.init({
-    clientId,
-    scope: scopes,
-  })
-}
-
-function handleLogin() {
-  gapi.load('client:auth2', () => {
-    gapi.auth2.init({ client_id: clientId }).then(() => {
-      const authInstance = gapi.auth2.getAuthInstance()
-      authInstance.signIn().then(user => {
-        const token = user.getAuthResponse().access_token
-        console.log('Access Token:', token)
-        // TODO: Use this token to call Gmail API
-      })
-    })
-  })
-}
+const userEmail = ref('')
 
 onMounted(() => {
-  gapi.load('client:auth2', start)
+  const waitForGoogle = setInterval(() => {
+    if (window.google && window.google.accounts && window.google.accounts.id) {
+      clearInterval(waitForGoogle)
+
+      window.google.accounts.id.initialize({
+        client_id: '343822659955-sjqmuqskg0s15buvhfm8ndj0u0p6hcba.apps.googleusercontent.com',
+        callback: handleCredentialResponse,
+        auto_select: false,
+        context: 'signin'
+      })
+
+      window.google.accounts.id.renderButton(
+        document.getElementById("g_id_signin"),
+        { theme: "outline", size: "large" }
+      )
+    }
+  }, 100)
 })
+
+
+function handleCredentialResponse(response) {
+  const token = response.credential
+  console.log("ID Token:", token)
+  // You can decode this token if needed using jwt.io or your backend
+}
 </script>
 
 <template>
-  <button @click="handleLogin">Login</button>
+  <div>
+    <div id="g_id_signin"></div>
+  </div>
 </template>
